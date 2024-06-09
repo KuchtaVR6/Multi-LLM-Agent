a3d3566a92ae7013ca171b7e95e3e9033b89c735
KuchtaVR6 <kuchtavr6@gmail.com>
2024-06-09 16:11:43 +0100

    api stats

--- caller_data.sh
+++ caller_data.sh
@@ -1,23 +1,13 @@
 cd ./GLPFT
 
 RAW_DATA_OUT_DIR="dataset/toolbench/train/raw_data"
-TRAIN_DATA_OUT_DIR="dataset/toolbench/train_mine"
+TRAIN_DATA_OUT_DIR="dataset/toolbench/train_separated/"
 export PYTHONPATH=./
-
-
-#python process_data/toolbench/prepro_raw_stage_1.py \
-# --data_dir $ORI_DATA_DIR \
-# --output_path $RAW_DATA_OUT_DIR
-#
-#
-#python process_data/toolbench/prepro_raw_stage_2.py \
-# --input_path $RAW_DATA_OUT_DIR/raw_data_stage_1.json \
-# --output_path $RAW_DATA_OUT_DIR
 
 for MODE in 'caller'
 do
     python process_data/toolbench/prepro_$MODE.py \
         --input_path $RAW_DATA_OUT_DIR/raw_data_stage_2.json \
-        --output_path $TRAIN_DATA_OUT_DIR/train_$MODE.json \
+        --output_path $TRAIN_DATA_OUT_DIR \
         --prompt_type toolbench_$MODE
 done
