578106244effb5503160260129810d8dc50a80f9
KuchtaVR6 <kuchtavr6@gmail.com>
2024-06-11 13:28:26 +0100

    updating variables to fit the new disc situation

--- job_creator.sh
+++ job_creator.sh
@@ -6,7 +6,7 @@
 
 # Define the job script file name based on the first argument
 job_script="${1}_job.sh"
-inner_script="${1}_inner.sh"
+inner_script="${1}_api.sh"
 
 # clear file contents
 > "$job_script"
@@ -15,16 +15,12 @@ inner_script="${1}_inner.sh"
 echo "#!/bin/bash" >> "$job_script"
 echo "" >> "$job_script"
 
-# Append the script contents to the job script file
-echo "export NNODE=${2} # num of GPUs" >> "$job_script"
-echo "" >> "$job_script"
-
 echo "# Grid Engine options" >> "$job_script"
 echo "#\$ -N ${1}  # Name of the job" >> "$job_script"
-echo "#\$ -cwd           # Run the job from the current working directory" >> "$job_script"
-echo "#\$ -l h_rt=10:00:00  # Request a runtime" >> "$job_script"
+echo "#\$ -wd /exports/eddie/scratch/s2595201/Multi-LLM-Agent # Run the job from the scratch directory" >> "$job_script"
+echo "#\$ -l h_rt=6:00:00  # Request a runtime" >> "$job_script"
 echo "#\$ -q gpu          # Submit the job to the gpu queue" >> "$job_script"
-echo "#\$ -pe gpu-a100 ${2}  # Request NNODE A100 GPUs" >> "$job_script"
+echo "#\$ -pe gpu-a100 1  # Request NNODE A100 GPUs" >> "$job_script"
 echo "#\$ -l h_vmem=80G    # Request memory per core" >> "$job_script"
 echo "" >> "$job_script"
 
@@ -36,14 +32,22 @@ echo "# Load the CUDA module" >> "$job_script"
 echo "module load cuda" >> "$job_script"
 echo "" >> "$job_script"
 
-echo "# point hugging_face to the right place" >> "$job_script"
-echo "export TRANSFORMERS_CACHE=/exports/eddie/scratch/s2595201/hugging_face_cache" >> "$job_script"
+echo "# Activate the conda environment for CUDA" >> "$job_script"
+echo "source /exports/csce/eddie/inf/groups/dawg/miniconda3/bin/activate base" >> "$job_script"
+
+echo "conda activate lean_env" >> "$job_script"
 echo "" >> "$job_script"
 
-echo "# Activate the conda environment for CUDA" >> "$job_script"
-echo "source ../miniconda3/bin/activate base" >> "$job_script"
+echo "export TOKENIZERS_PARALLELIZM=false" >> "$job_script"
+echo "" >> "$job_script"
 
-echo "conda activate umi${3}" >> "$job_script"
+echo "export HF_HOME=/exports/csce/eddie/inf/groups/dawg/HuggingFaceCache" >> "$job_script"
+echo "" >> "$job_script"
+
+echo "export TRANSFORMERS_CACHE=/exports/csce/eddie/inf/groups/dawg/HuggingFaceCache" >> "$job_script"
+echo "" >> "$job_script"
+
+echo "huggingface-cli login --token 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 --add-to-git-credential" >> "$job_script"
 echo "" >> "$job_script"
 
 cat "$inner_script" >> "$job_script"
