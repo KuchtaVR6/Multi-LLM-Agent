b122e58271646f4f8acb63445326941707170412
KuchtaVR6 <kuchtavr6@gmail.com>
2024-06-16 17:04:10 +0100

    making the base infrence more verbose for the patches inference

--- inference_inner.sh
+++ inference_inner.sh
@@ -7,7 +7,7 @@ CAL_PATH="shenwzh3/alpha-umi-caller-7b"
 SUM_PATH="shenwzh3/alpha-umi-summarizer-7b"
 
 
-LAB_DIR=output_res/toolbench
+LAB_DIR=output_res_partial/toolbench
 P_TYPE_PLAN=toolbench_planner
 P_TYPE_CAL=toolbench_caller
 P_TYPE_SUM=toolbench_summarizer
@@ -15,7 +15,7 @@ P_TYPE_SUM=toolbench_summarizer
 for DOMAIN in 'in_domain'
 do
     export PYTHONPATH=./
-    torchrun --nproc_per_node=$NNODE --master_port=$PORT inference_utils/toolbench/infer_pipeline.py \
+    python inference_utils/toolbench/infer_pipeline.py \
         --planner_model_name_or_path $PLAN_PATH  \
         --planner_use_lora False \
         --caller_model_name_or_path $CAL_PATH  \
@@ -27,12 +27,9 @@ do
         --bf16_full_eval \
         --planner_prompt_type $P_TYPE_PLAN \
         --caller_prompt_type $P_TYPE_CAL \
-	--summarizer_prompt_type $P_TYPE_SUM \
+	      --summarizer_prompt_type $P_TYPE_SUM \
         --max_input_length 3750 \
         --output_dir $LAB_DIR/$DOMAIN \
-
-    python inference_utils/toolbench/evaluate-multi_agent.py \
-    --input_path $LAB_DIR/$DOMAIN/predictions.json \
-    --output_path $LAB_DIR/$DOMAIN/metrics.json
+        --num_infer_samples 2 \
 
 done
