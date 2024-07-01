7322c165ad8580f900c62e2879b327dc85be9164
KuchtaVR6 <kuchtavr6@gmail.com>
2024-07-01 16:41:33 +0100

    refactoring and refinements

--- train_api_creator.py
+++ train_api_creator.py
@@ -1,94 +1,24 @@
+#!/usr/bin/env python3
+
 import os
-import subprocess
 import json
 import sys
 
-def main(API_NAME, MODEL='caller', JOB='job', ALL_APIS=False):
-    os.chdir('GLPFT')
 
-    # Parameters
-    FILENAME = f"../inner_scripts/{JOB}_api"
-    CERTAINTY = 'all' if ALL_APIS else 'certain'
-
-    # Default settings
-    USE_LORA = True
-    BSZ = 4
-    GA = 8 // BSZ  # Ensure BSZ * GA = 8
-    CONTEXT_LENGTH = 4096
-
-    CATEGORY = API_NAME[0].isupper()
-    API_NAME = API_NAME.lower()
-
-    if CATEGORY:
-        DATA_PATH = f"dataset/toolbench/new_data/{CERTAINTY}/category/{API_NAME}_train.json"
-    elif "_for_" in API_NAME:
-        DATA_PATH = f"dataset/toolbench/new_data/{CERTAINTY}/endpoint/{API_NAME}_train.json"
-    else:
-        DATA_PATH = f"dataset/toolbench/new_data/{CERTAINTY}/api_family/{API_NAME}_train.json"
-
-    EXP_NAME = f"output_patches/{API_NAME}/"
-
-    if MODEL == 'dev':
-        INPUT_MODEL = "EleutherAI/pythia-160m"
-        CONTEXT_LENGTH = 2048
-        TARGET_MODULES = "query_key_value"
-        FILENAME = f"../inner_scripts/{API_NAME}_dev_api"
-        EXP_NAME = f"output_patches/{API_NAME}_dev/"
-    elif MODEL == 'backbone':
-        INPUT_MODEL = "saved_models/backbone"
-        EXP_NAME = f"output_patches/{API_NAME}_backbone/"
-        FILENAME = f"../inner_scripts/{API_NAME}_backbone_api"
-    elif MODEL == 'llama':
-        INPUT_MODEL = "meta-llama/Llama-2-7b-hf"
-        FILENAME = f"../inner_scripts/{API_NAME}_llama_api"
-        EXP_NAME = f"output_patches/{API_NAME}_llama/"
-    else:
-        INPUT_MODEL = "shenwzh3/alpha-umi-caller-7b"
-        EXP_NAME = f"output_patches/{API_NAME}/"
-        FILENAME = f"../inner_scripts/{API_NAME}_api"
-
-    if CERTAINTY == 'all':
-        EXP_NAME = EXP_NAME.rstrip('/') + '_all/'
-        FILENAME += '_all'
-
-    print("=====================")
-    print(f"API_NAME: {API_NAME}")
-    print(f"MODEL: {MODEL}")
-    print(f"INPUT_MODEL: {INPUT_MODEL}")
-    print(f"FILENAME: {FILENAME}")
-    print(f"EXP_NAME: {EXP_NAME}")
-    print(f"DATA_PATH: {DATA_PATH}")
-    print(f"CONTEXT_LENGTH: {CONTEXT_LENGTH}")
-    print(f"BSZ: {BSZ}")
-    print(f"GA: {GA}")
-    print(f"USE_LORA: {USE_LORA}")
-    print("=====================")
-
-    try:
-        with open(DATA_PATH, 'r') as f:
-            data = json.load(f)
-        NUM_SAMPLES = len(data)
-    except FileNotFoundError:
-        print(f"File not found: {DATA_PATH}")
-        sys.exit(1)
-
-    MIN_SAMPLES = 10000
-    EPOCHS = (MIN_SAMPLES + NUM_SAMPLES - 1) // NUM_SAMPLES
-
-    os.chdir('..')
-
+def create_script_content(input_model, data_path, exp_name, epochs, bsz, ga, context_length, use_lora, filename,
+                          target_modules=None):
     script_content = f"""#!/bin/bash
 
 cd GLPFT
 
 python train_mine.py \\
-    --model_name_or_path {INPUT_MODEL} \\
-    --data_path {DATA_PATH} \\
-    --output_dir {EXP_NAME} \\
-    --num_train_epochs {EPOCHS} \\
-    --per_device_train_batch_size {BSZ} \\
-    --per_device_eval_batch_size {BSZ} \\
-    --gradient_accumulation_steps {GA} \\
+    --model_name_or_path {input_model} \\
+    --data_path {data_path} \\
+    --output_dir {exp_name} \\
+    --num_train_epochs {epochs} \\
+    --per_device_train_batch_size {bsz} \\
+    --per_device_eval_batch_size {bsz} \\
+    --gradient_accumulation_steps {ga} \\
     --eval_strategy "no" \\
     --eval_steps 0 \\
     --save_strategy "steps" \\
@@ -99,36 +29,112 @@ python train_mine.py \\
     --lr_scheduler_type "cosine" \\
     --gradient_checkpointing True \\
     --logging_steps 2 \\
-    --model_max_length {CONTEXT_LENGTH} \\
+    --model_max_length {context_length} \\
     --report_to none \\
     --lazy_preprocess False \\
     --bf16 True \\
-    --lora {USE_LORA} \\
+    --lora {use_lora} \\
 """
 
-    if USE_LORA and 'TARGET_MODULES' in locals():
-        script_content += f"    --lora_target_modules {TARGET_MODULES} \\\n"
+    if use_lora and target_modules:
+        script_content += f"    --lora_target_modules {target_modules} \\\n"
 
     script_content += '\n'
 
-    with open(f"{FILENAME}.sh", 'w') as f:
+    with open(f"{filename}.sh", 'w') as f:
         f.write(script_content)
 
-    os.chmod(f"{FILENAME}.sh", 0o755)
+    os.chmod(f"{filename}.sh", 0o755)
 
-    SAMPLE_COUNT = NUM_SAMPLES * EPOCHS
 
-    print(f"Script {FILENAME}.sh has been created and made executable. It is configured to train the model '{MODEL}' using data for the API '{API_NAME}'. {SAMPLE_COUNT} training samples will be seen during training.")
+def main(api_name, model='caller', all_apis=False):
+    os.chdir('GLPFT')
+
+    certainty = 'all' if all_apis else 'certain'
+
+    # Default settings
+    use_lora = True
+    bsz = 4
+    ga = 8 // bsz  # Ensure BSZ * GA = 8
+    context_length = 4096
+
+    category = api_name[0].isupper()
+    api_name = api_name.lower()
+
+    if category:
+        data_path = f"dataset/toolbench/new_data/{certainty}/category/{api_name}_train.json"
+    elif "_for_" in api_name:
+        data_path = f"dataset/toolbench/new_data/{certainty}/endpoint/{api_name}_train.json"
+    else:
+        data_path = f"dataset/toolbench/new_data/{certainty}/api_family/{api_name}_train.json"
+
+    exp_name = f"output_patches/{api_name}/"
+
+    model_settings = {
+        'dev': ("EleutherAI/pythia-160m", 2048, "query_key_value", f"../inner_scripts/{api_name}_dev_api",
+                f"output_patches/{api_name}_dev/"),
+        'backbone': ("saved_models/backbone", 4096, None, f"../inner_scripts/{api_name}_backbone_api",
+                     f"output_patches/{api_name}_backbone/"),
+        'llama': ("meta-llama/Llama-2-7b-hf", 4096, None, f"../inner_scripts/{api_name}_llama_api",
+                  f"output_patches/{api_name}_llama/"),
+        "caller[vimeo]": (
+        "placeholder_model", "placeholder_context_length", "placeholder_target_modules", "placeholder_filename",
+        "placeholder_experiment_name")
+    }
+
+    if model in model_settings:
+        input_model, context_length, target_modules, filename, exp_name = model_settings[model]
+    else:
+        input_model = "shenwzh3/alpha-umi-caller-7b"
+        filename = f"../inner_scripts/{api_name}_api"
+
+    if certainty == 'all':
+        exp_name = exp_name.rstrip('/') + '_all/'
+        filename += '_all'
+
+    print("=====================")
+    print(f"API_NAME: {api_name}")
+    print(f"MODEL: {model}")
+    print(f"INPUT_MODEL: {input_model}")
+    print(f"FILENAME: {filename}")
+    print(f"EXP_NAME: {exp_name}")
+    print(f"DATA_PATH: {data_path}")
+    print(f"CONTEXT_LENGTH: {context_length}")
+    print(f"BSZ: {bsz}")
+    print(f"GA: {ga}")
+    print(f"USE_LORA: {use_lora}")
+    print("=====================")
+
+    try:
+        with open(data_path, 'r') as f:
+            data = json.load(f)
+        num_samples = len(data)
+    except FileNotFoundError:
+        print(f"File not found: {data_path}")
+        sys.exit(1)
+
+    min_samples = 10000
+    epochs = (min_samples + num_samples - 1) // num_samples
+
+    os.chdir('..')
+
+    create_script_content(input_model, data_path, exp_name, epochs, bsz, ga, context_length, use_lora, filename,
+                          locals().get('target_modules'))
+
+    sample_count = num_samples * epochs
+
+    print(
+        f"Script {filename}.sh has been created and made executable. It is configured to train the model '{model}' using data for the API '{api_name}'. {sample_count} training samples will be seen during training.")
+
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='Translate Bash script to Python.')
-    parser.add_argument('API_NAME', type=str, help='Name of the API')
-    parser.add_argument('MODEL', type=str, nargs='?', default='caller', help='Model to use')
-    parser.add_argument('JOB', type=str, nargs='?', default='job', help='Job filename')
-    parser.add_argument('ALL_APIS', type=bool, nargs='?', default=False, help='Use all APIs or certain APIs')
+    parser.add_argument('api_name', type=str, help='Name of the API')
+    parser.add_argument('model', type=str, nargs='?', default='caller', help='Model to use')
+    parser.add_argument('all_apis', type=bool, nargs='?', default=False, help='Use all APIs or certain APIs')
 
     args = parser.parse_args()
 
-    main(args.API_NAME, args.MODEL, args.JOB, args.ALL_APIS)
+    main(args.api_name, args.model, args.all_apis)
