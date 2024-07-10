09c18f202bf6fc6dd82b82d2254d380fc9a58830
KuchtaVR6 <kuchtavr6@gmail.com>
2024-07-10 15:17:28 +0100

    evaluate-expert-improvements as a function

--- GLPFT/inference_utils/toolbench/evaluate-expert-improvements.py
+++ GLPFT/inference_utils/toolbench/evaluate-expert-improvements.py
@@ -1,173 +1,145 @@
 import json
-import argparse
-
 import os
 from collections import defaultdict
 
 from multi_agent_utils import (evaluate_rougel, evaluate_reasoning, evaluate_action_em, evaluate_action_input_f1,
                                count_unique_strings, display_counts, sort_and_display_dict, parse_output)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--input_path_expert', type=str, default="")
-parser.add_argument('--input_path_backoff', type=str, default="")
-parser.add_argument('--id_sample_matching', type=bool, default=False)
-parser.add_argument('--output_path', type=str, default="")
-parser.add_argument('--only_certain', type=bool, default=False)
 
-args = parser.parse_args()
+def evaluate(input_path_expert, input_path_backoff, output_path, id_sample_matching=False, only_certain=False):
+    with open(input_path_expert, encoding='utf-8') as f:
+        expert_data = json.load(f)
 
-with open(args.input_path_expert, encoding='utf-8') as f:
-    expert_data = json.load(f)
-
-with open(args.input_path_backoff, encoding='utf-8') as f:
-    if args.id_sample_matching:
-        ids_in = []
-        for entry in expert_data:
-            ids_in.append(entry['caller_sample_id'])
-        raw_data = json.load(f)
-        original_data = []
-        for entry in raw_data:
-            if entry['caller_sample_id'] in ids_in:
-                original_data.append(entry)
-    else:
-        original_data = json.load(f)
-
-print(len(original_data), len(expert_data))
-
-for current_data in [original_data, expert_data]:
-    plan_ref = []
-    plan_pred = []
-    hallu_cases = []
-    error_cases = []
-    new_data = []
-    answer_ref = []
-    action_ref = []
-    action_input_ref = []
-    hallu_ref = 0
-    answer_pred = []
-    action_pred = []
-    action_input_pred = []
-    hallu_pred = 0
-
-    caller_stats = defaultdict(list)
-    reasoning_stats = defaultdict(list)
-
-    print(len(current_data))
-
-    for d in current_data:
-        reference = d['reference']
-        prediction = d['predictions']
-
-        tools_available = [t['Name'] for t in d['tools']]
-
-        ref_plan, ref_action, ref_input, ref_ans, ref_reason = parse_output(reference)
-        pred_plan, pred_action, pred_input, pred_ans, pred_reason = parse_output(prediction)
-
-        if pred_action is not None and pred_action != 'none' and pred_action not in tools_available:
-            hallu_pred += 1
-            hallu_cases.append(d)
-        plan_ref.append(ref_plan)
-        plan_pred.append(pred_plan)
-        if ref_plan == 'give up':
-            pass
-        elif ref_plan == 'finish':
-            answer_ref.append(ref_ans)
-            if pred_ans is None:
-                answer_pred.append('none')
-            else:
-                answer_pred.append(pred_ans)
+    with open(input_path_backoff, encoding='utf-8') as f:
+        if id_sample_matching:
+            ids_in = [entry['caller_sample_id'] for entry in expert_data]
+            raw_data = json.load(f)
+            original_data = [entry for entry in raw_data if entry['caller_sample_id'] in ids_in]
         else:
-            ref_validity = evaluate_reasoning(ref_reason, ref_action, tools_available)
-            if ref_validity != 'correct' and args.only_certain:
+            original_data = json.load(f)
+
+    for current_data in [original_data, expert_data]:
+        plan_ref = []
+        plan_pred = []
+        hallu_cases = []
+        error_cases = []
+        new_data = []
+        answer_ref = []
+        action_ref = []
+        action_input_ref = []
+        hallu_ref = 0
+        answer_pred = []
+        action_pred = []
+        action_input_pred = []
+        hallu_pred = 0
+
+        caller_stats = defaultdict(list)
+        reasoning_stats = defaultdict(list)
+
+        for d in current_data:
+            reference = d['reference']
+            prediction = d['predictions']
+            tools_available = [t['Name'] for t in d['tools']]
+
+            ref_plan, ref_action, ref_input, ref_ans, ref_reason = parse_output(reference)
+            pred_plan, pred_action, pred_input, pred_ans, pred_reason = parse_output(prediction)
+
+            if pred_action and pred_action != 'none' and pred_action not in tools_available:
+                hallu_pred += 1
+                hallu_cases.append(d)
+            plan_ref.append(ref_plan)
+            plan_pred.append(pred_plan)
+            if ref_plan == 'give up':
+                pass
+            elif ref_plan == 'finish':
+                answer_ref.append(ref_ans)
+                answer_pred.append(pred_ans if pred_ans else 'none')
+            else:
+                ref_validity = evaluate_reasoning(ref_reason, ref_action, tools_available)
+                if ref_validity != 'correct' and only_certain:
+                    continue
+
+                caller_stats['instances'].append(ref_action)
+                if ref_action == pred_action:
+                    caller_stats['correct_action'].append(ref_action)
+                else:
+                    if not pred_action:
+                        caller_stats['no_action_against_expectation'].append(ref_action)
+                    elif not ref_action:
+                        caller_stats['action_against_expectation_of_none'].append(ref_action)
+                    else:
+                        caller_stats['wrong_action'].append(ref_action)
+
+                    reasoning_stats['ref valid?'].append(ref_validity)
+                    reasoning_stats['pred res valid?'].append(evaluate_reasoning(pred_reason, ref_action, tools_available))
+                    reasoning_stats['pred reasonable action?'].append(evaluate_reasoning(pred_reason, pred_action, tools_available))
+
+                action_ref.append(ref_action)
+                action_input_ref.append(ref_input)
+                action_pred.append(pred_action if pred_action else 'none')
+                action_input_pred.append(pred_input if pred_input else '{}')
+
+        metric = {}
+        rouge = evaluate_rougel(answer_pred, answer_ref)
+        plan_em = evaluate_action_em(plan_ref, plan_pred)
+
+        action_em, action_em_per_ref = evaluate_action_em(action_ref, action_pred)
+        easy_f1_dict, hard_f1_dict, f1_dict = evaluate_action_input_f1(action_pred, action_ref, action_input_pred, action_input_ref)
+
+        easy_f1_list = [item for sublist in easy_f1_dict.values() for item in sublist]
+        hard_f1_list = [item for sublist in hard_f1_dict.values() for item in sublist]
+        f1_list = [item for sublist in f1_dict.values() for item in sublist]
+
+        easy_f1 = sum(easy_f1_list) / (len(easy_f1_list) + 1e-30)
+        hard_f1 = sum(hard_f1_list) / (len(hard_f1_list) + 1e-30)
+        f1 = sum(f1_list) / (len(f1_list) + 1e-30)
+
+        hallu_rate = hallu_pred / len(current_data)
+        metric['rouge'] = rouge
+        metric['plan_em'] = plan_em
+        metric['action_em'] = action_em
+        metric['easy_f1'] = easy_f1
+        metric['hard_f1'] = hard_f1
+        metric['f1'] = f1
+        metric['hallu_rate'] = hallu_rate
+
+        if not os.path.exists(os.path.dirname(output_path)):
+            os.makedirs(os.path.dirname(output_path))
+        with open(output_path, 'w', encoding='utf-8') as f:
+            json.dump(metric, f, indent=2)
+
+        with open(output_path.replace('metrics.json', 'hallu_cases.json'), 'w', encoding='utf-8') as f:
+            json.dump(hallu_cases, f, indent=2)
+
+        print('F1 INFORMATION')
+        print('name,em,f1,hard_f1,easy_f1,adj_f1,adj_hard_f1,adj_easy_f1')
+        for ref in f1_dict.keys():
+            f1_data = f1_dict[ref]
+            hard_f1_data = hard_f1_dict[ref]
+            easy_f1_data = easy_f1_dict[ref]
+
+            f1 = sum(f1_data) / (len(f1_data) + 1e-30)
+            hard_f1 = sum(hard_f1_data) / (len(hard_f1_data) + 1e-30)
+            easy_f1 = sum(easy_f1_data) / (len(easy_f1_data) + 1e-30)
+
+            em = action_em_per_ref.get(ref, 1)
+
+            if em == 0 or em == 1 or f1 + hard_f1 + easy_f1 == 0:
                 continue
 
-            caller_stats['instances'].append(ref_action)
-            if ref_action == pred_action:
-                caller_stats['correct_action'].append(ref_action)
-            else:
-                if not pred_action:
-                    caller_stats['no_action_against_expectation'].append(ref_action)
-                elif not ref_action:
-                    caller_stats['action_against_expectation_of_none'].append(ref_action)
-                else:
-                    caller_stats['wrong_action'].append(ref_action)
-
-                reasoning_stats['ref valid?'].append(ref_validity)
-                reasoning_stats['pred res valid?'].append(evaluate_reasoning(pred_reason, ref_action, tools_available))
-                reasoning_stats['pred reasonable action?'].append(evaluate_reasoning(pred_reason, pred_action, tools_available))
-
-            action_ref.append(ref_action)
-            action_input_ref.append(ref_input)
-            if pred_action is None:
-                action_pred.append('none')
-            else:
-                action_pred.append(pred_action)
-
-            if pred_input is None:
-                action_input_pred.append('{}')
-            else:
-                action_input_pred.append(pred_input)
-
-    metric = {}
-    rouge = evaluate_rougel(answer_pred, answer_ref)
-    plan_em = evaluate_action_em(plan_ref, plan_pred)
-
-    action_em, action_em_per_ref = evaluate_action_em(action_ref, action_pred)
-    easy_f1_dict, hard_f1_dict, f1_dict = evaluate_action_input_f1(action_pred, action_ref, action_input_pred, action_input_ref)
-
-    easy_f1_list = [item for sublist in easy_f1_dict.values() for item in sublist]
-    hard_f1_list = [item for sublist in hard_f1_dict.values() for item in sublist]
-    f1_list = [item for sublist in f1_dict.values() for item in sublist]
-
-    easy_f1 = sum(easy_f1_list) / len(easy_f1_list)+1e-30
-    hard_f1 = sum(hard_f1_list) / len(hard_f1_list)+1e-30
-    f1 = sum(f1_list) / len(f1_list)+1e-30
-
-    hallu_rate = hallu_pred / len(current_data)
-    metric['rouge'] = rouge
-    metric['plan_em'] = plan_em
-    metric['action_em'] = action_em
-    metric['easy_f1'] = easy_f1
-    metric['hard_f1'] = hard_f1
-    metric['f1'] = f1
-    metric['hallu_rate'] = hallu_rate
+            print(f'{ref},{em:.2f},{f1:.2f},{hard_f1:.2f},{easy_f1:.2f}')
 
 
-    if not os.path.exists(os.path.dirname(args.output_path)):
-        os.makedirs(os.path.dirname(args.output_path))
-    print(metric)
+if __name__ == "__main__":
+    import argparse
 
-    with open(args.output_path, 'w', encoding='utf-8') as f:
-        json.dump(metric,f, indent=2)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_path_expert', type=str, default="")
+    parser.add_argument('--input_path_backoff', type=str, default="")
+    parser.add_argument('--id_sample_matching', type=bool, default=False)
+    parser.add_argument('--output_path', type=str, default="")
+    parser.add_argument('--only_certain', type=bool, default=False)
 
-    with open(args.output_path.replace('metrics.json', 'hallu_cases.json'), 'w', encoding='utf-8') as f:
-        json.dump(hallu_cases,f, indent=2)
+    args = parser.parse_args()
 
-
-    # display_counts(count_unique_strings(caller_stats))
-    # print('---')
-    # display_counts(count_unique_strings(reasoning_stats))
-    # print('---')
-    # sort_and_display_dict(action_em_per_ref, skip_zeros=True)
-    # print('---')
-
-    print('F1 INFORMATION')
-    print('name,em,f1,hard_f1,easy_f1,adj_f1,adj_hard_f1,adj_easy_f1')
-    for ref in f1_dict.keys():
-        f1_data = f1_dict[ref]
-        hard_f1_data = hard_f1_dict[ref]
-        easy_f1_data = easy_f1_dict[ref]
-
-        f1 = sum(f1_data) / (len(f1_data) + 1e-30)
-        hard_f1 = sum(hard_f1_data) / (len(hard_f1_data) + 1e-30)
-        easy_f1 = sum(easy_f1_data) / (len(easy_f1_data) + 1e-30)
-
-        if ref in action_em_per_ref:
-            em = action_em_per_ref[ref]
-        else:
-            em = 1
-
-        if em == 0 or em == 1 or f1 + hard_f1 + easy_f1 == 0:
-            continue
-
-        print(f'{ref},{em:.2f},{f1:.2f},{hard_f1:.2f},{easy_f1:.2f}')
+    evaluate(args.input_path_expert, args.input_path_backoff, args.output_path, args.id_sample_matching, args.only_certain)
