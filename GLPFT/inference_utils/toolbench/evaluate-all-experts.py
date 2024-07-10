a0212aba2f18d9b473ccbc2595cd4e3e79ba67cc
KuchtaVR6 <kuchtavr6@gmail.com>
2024-07-10 20:03:08 +0100

    theoretically completed implementation of the evaluate all experts

--- GLPFT/inference_utils/toolbench/evaluate-all-experts.py
+++ GLPFT/inference_utils/toolbench/evaluate-all-experts.py
@@ -15,18 +15,31 @@ if __name__ == "__main__":
     # specific datasets
 
     for test_type in ['all', 'certain']:
-        with open(f'test_results_{test_type}.txt', 'w') as file:
+        with open(os.path.join(args.output_path + f'test_specific_results_{test_type}.txt', 'w')) as file:
             def output(line):
                 file.write(line)
 
             for dir_path, dir_names, filenames in os.walk(args.input_path_folder):
                 target_filename = f'{test_type}_expert_predictions.json'
                 if target_filename in filenames:
+                    file.write(f'Adapter: {dir_path}')
                     evaluate(os.path.join(dir_path, target_filename), args.input_path_backoff, args.output_path,
                              False, False, output_func=output)
 
 
-    # toolbench dataset TODO
+    # toolbench dataset
 
+    with open(os.path.join(args.output_path + f'test_toolbench_results.txt', 'w')) as file:
+        def output(line):
+            file.write(line)
+
+        for dir_path, dir_names, filenames in os.walk(args.input_path_folder):
+            target_filename = f'toolbench_expert_predictions.json'
+            if target_filename in filenames:
+                file.write(f'Adapter: {dir_path}')
+                evaluate(os.path.join(dir_path, target_filename), args.input_path_backoff, args.output_path,
+                         False, False, output_func=output)
     evaluate(args.input_path_expert, args.input_path_backoff, args.output_path, True, False)
+
+
 
