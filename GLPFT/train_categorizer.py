c641d6ac4281cd40d2ce35cdb8e05f6d458c1115
KuchtaVR6 <kuchtavr6@gmail.com>
2024-07-23 19:30:06 +0100

    array fix

--- GLPFT/train_categorizer.py
+++ GLPFT/train_categorizer.py
@@ -7,6 +7,7 @@ from transformers import LongformerModel, LongformerTokenizer, AdamW
 from tqdm import tqdm
 import argparse
 from utils.data_split import train_validation_split
+import numpy as np
 
 
 # Load data from a JSON file
@@ -30,7 +31,7 @@ def tokenize_data(data, tokenizer):
         )
         inputs.append(encoded_input)
         targets.append(item['target'])
-    return inputs, targets
+    return np.array(inputs), np.array(targets)
 
 
 class TextClassificationDataset(Dataset):
