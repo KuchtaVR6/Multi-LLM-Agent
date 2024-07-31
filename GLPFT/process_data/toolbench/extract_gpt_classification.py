c394bb7e7b7e8aa6f5dd20cd1f8192edd7f8f6dc
KuchtaVR6 <kuchtavr6@gmail.com>
2024-07-31 12:57:24 +0100

    duplication prevention

--- GLPFT/process_data/toolbench/extract_gpt_classification.py
+++ GLPFT/process_data/toolbench/extract_gpt_classification.py
@@ -1,19 +1,11 @@
 import json
 
+from process_data.toolbench.ood_data_annotation_prompts import categories
+
 # Path to the JSON Lines file
 file_path = 'output_verbose_res/outputs_classification.jsonl'
 # Path to the output file
 output_file_path = 'dataset/toolbench/toolalpaca_api_categories.txt'
-
-# List of valid categories
-categories = ['Logistics', 'Cryptography', 'Jobs', 'Gaming', 'Social', 'Sports', 'Database', 'Business_Software',
-              'Music', 'Business', 'Location', 'Travel', 'Artificial_Intelligence_Machine_Learning', 'Science', 'Email',
-              'Events', 'Health_and_Fitness', 'Payments', 'Movies', 'Text_Analysis', 'Transportation', 'Monitoring',
-              'Medical', 'Financial', 'Weather', 'Video_Images', 'Devices', 'Customized', 'SMS', 'Food',
-              'Entertainment', 'Advertising', 'Energy', 'Tools', 'Search', 'Media', 'eCommerce',
-              'Visual_Recognition', 'Data', 'Communication', 'Other', 'Finance', 'Cybersecurity', 'News_Media',
-              'Translation', 'Mapping', 'Commerce', 'Storage', 'Reward', 'Education', 'Unknown']
-
 
 def extract_and_save_info(file_path, output_file_path):
     with open(file_path, 'r') as file, open(output_file_path, 'w') as output_file:
